47cc51ae01ae973bcdb8a452fb03db3044d9f8ed
HiLaVine <sinnafa01@gmail.com>
2023-05-22 08:39:36 -0600

    PHP

---
+++ php/confirmacion.php
@@ -0,0 +1,10 @@
+<?php 
+    $nombre = $_POST["name"]; 
+    $Apaterno = $_POST["Lname"]; 
+    $Amaterno= $_POST["Mname"];
+    $contraseña = $_POST["pass"];
+    echo $nombre;
+    echo $Apaterno;
+    echo $Amaterno;
+    echo $contraseña;
+?>
